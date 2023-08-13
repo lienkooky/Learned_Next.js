9e364a164520a3019237f3571e3175630f8ce47b
lien <lienkooky@git.com>
2023-08-13 21:42:52 +0900

    how to coding cache

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -1,3 +1,20 @@
-export default function Home() {
-  return <h1>hello</h1>;
+type Time = {
+  datetime: string;
+};
+
+async function getTime(): Promise<Time> {
+  const res = await fetch(
+    "http://worldtimeapi.org/api/timezone/America/Chicago",
+    { next: { revalidate: 3 } }
+  );
+  return res.json();
+}
+
+export default async function Home() {
+  const [time] = await Promise.all([getTime()]);
+  return (
+    <div>
+      <h1>{time.datetime}</h1>
+    </div>
+  );
 }
